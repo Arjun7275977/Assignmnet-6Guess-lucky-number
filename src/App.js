b6126844162352f829d44bfa8b1ed7f732e543fd
Anurag7275977 <arjunsharma705463@gmail.com>
2023-05-01 21:43:06 +0530

    UseRef Assignmnet Completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
 import GuessLuckyNumber from './Component/GuessLuckyNumber.js';
+import UsingRef from './Component/useref'
 function App() {
   return (
     <div className="App">
       <GuessLuckyNumber/>
+      <UsingRef/>
     </div>
   );
 }
