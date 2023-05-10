c19a804a1e0330f2df5d40a5c441fe265dff212f
Anurag7275977 <arjunsharma705463@gmail.com>
2023-05-11 01:46:16 +0530

    Scrolebar assisgnment is done

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@
 import './App.css';
 import GuessLuckyNumber from './Component/GuessLuckyNumber.js';
 import UsingRef from './Component/useref'
+import Scroller from './Scrolebar/scrole';
+
 function App() {
   return (
     <div className="App">
-      <GuessLuckyNumber/>
-      <UsingRef/>
+      {/* <GuessLuckyNumber/>
+      <UsingRef/> */}
+      <Scroller/>
     </div>
   );
 }
---
+++ src/Scrolebar/scrole.js
@@ -0,0 +1,47 @@
+
+export default function Scroller(){
+
+    return(
+        
+ <>
+<h1>   functionup@ Management </h1>
+
+<div className='wrapper'>
+
+
+
+<div className='card'> 
+<img src="https://functionup.org/_next/image?url=%2F_next%2Fstatic%2Fimage%2Fpublic%2Falumnis%2Fsarthak.4e999c8bd30aa05493dffafd18e6eba6.jpeg&w=1920&q=75"></img>
+ <p> Sarthak Desai<br/> Goa Engineering College</p> 
+ </div>
+
+ <div className='card'> 
+ <img src="https://functionup.org/_next/image?url=http%3A%2F%2Fs3.amazonaws.com%2Fappforest_uf%2Ff1673072872170x308945872166266360%2Fsakshi_ds_t.jpeg&w=1920&q=75"></img>
+ <p>Sakshi Singh <br/> Earthinators Climate School</p> </div>
+
+
+ <div className='card'> 
+ <img src="https://functionup.org/_next/image?url=%2F_next%2Fstatic%2Fimage%2Fpublic%2Falumnis%2Frajat.5b6d53735ad750c18a3162a6f119bf66.jpeg&w=1920&q=75"></img> 
+  <p>Rajat Rout <br/>GTU Klub</p>  </div>
+
+   
+<div className='card'>
+     <img src="https://functionup.org/_next/image?url=http%3A%2F%2Fs3.amazonaws.com%2Fappforest_uf%2Ff1673073484887x914649679498532100%2Fsatya_1.jpeg&w=1920&q=75"></img>
+      <p>Satya Kirity PBS<br/> Lovely Professional University
+
+Technovert</p> </div>
+<div className='card'>
+     <img src="https://functionup.org/_next/image?url=http%3A%2F%2Fs3.amazonaws.com%2Fappforest_uf%2Ff1673072528271x366058927609036500%2F1663040418966-2.jpeg&w=1920&q=75"></img>
+      <p>Akash Giri <br/> ASSAM SCIENCE AND TECHNOLOGY UNIVERSITY</p>  </div>
+<div className='card'>
+    <img src="https://functionup.org/_next/image?url=http%3A%2F%2Fs3.amazonaws.com%2Fappforest_uf%2Ff1673073726612x370615787091414400%2Fakshat_1.jpeg&w=1920&q=75"></img> 
+    <p>Akshat Nishad <br/>Vinoba Bhave University </p> </div> 
+
+
+ </div>
+   
+   
+ </>
+
+    );
+}
